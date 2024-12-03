07bddc325a4c5ff708d8bbffe0fa20c9cb1d9a65
ismoil <ismoilhukumatov9990@gmail.com>
2024-12-03 10:53:01 +0500

    adding ui todo add

--- app/src/main/java/com/example/tzmegafon/ui/screens/main/MainFragment.kt
+++ app/src/main/java/com/example/tzmegafon/ui/screens/main/MainFragment.kt
@@ -1,4 +1,4 @@
-package com.example.tzmegafon
+package com.example.tzmegafon.ui.screens.main
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,14 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.tzmegafon.databinding.FragmentSecondBinding
+import com.example.tzmegafon.R
+import com.example.tzmegafon.databinding.FragmentMainBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +23,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +31,8 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.addTodo.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
